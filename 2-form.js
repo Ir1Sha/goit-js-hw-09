be0270e0a2712bf9dd6acacc20aac4a65136a55d
Ir1Sha <95133282+Ir1Sha@users.noreply.github.com>
2024-11-17 20:43:03 +0000

    Deploying to gh-pages from @ Ir1Sha/goit-js-hw-09@a14cf7756c63309db2b235c712cd5c597c31f1bb 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-import"./assets/main-CrTNQcx4.js";console.log("Form");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const l={email:"",message:""},s="feedback-form-state",t=document.querySelector(".feedback-form");t.addEventListener("input",m);t.addEventListener("submit",o);function m(e){l[e.target.name]=e.target.value,localStorage.setItem(s,JSON.stringify(l))}n();function o(e){e.preventDefault();const a=t.email.value.trim(),r=t.message.value.trim();if(!a||!r){alert("Fill please all fields");return}e.currentTarget.reset(),localStorage.removeItem(s)}function n(){const e=localStorage.getItem(s);if(e){const a=JSON.parse(e);t.email.value=a.email||"",t.message.value=a.message||""}}
 //# sourceMappingURL=2-form.js.map
